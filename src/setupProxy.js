dba06dbe969512c1da1352116681cfe58e416196
SkyPractice <SkyPractice982@gmail.com>
2025-03-20 15:19:29 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',  // The path to your backend API
     createProxyMiddleware({
-      target: 'https://your-backend-url.com',
+      target: 'https://skypractice.xyz:25622',
       changeOrigin: true,
       secure: false,  // This allows invalid SSL certificates (not recommended for production)
     })
